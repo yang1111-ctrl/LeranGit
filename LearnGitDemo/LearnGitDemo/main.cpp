94d26ed425b6fbb41a13c1730a0db2a1dbc780b8
Yang <zhangjiayang@zhangjiayangdeMacBook-Air.local>
2020-04-12 22:07:43 +0800

    Frist Commit

---
+++ LearnGitDemo/LearnGitDemo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LearnGitDemo
+//
+//  Created by 张佳炀 on 2020/4/12.
+//  Copyright © 2020 炀Man. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
